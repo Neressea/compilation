9e8f4dddd233bb82bf4b6a06db65b0a83f5b580c
Neressea <vincent.albert@telecomnancy.eu>
2016-03-30 19:57:01 +0200

    Correction tableau

--- Analyse_semantique/ControleAccesTableau.java
+++ Analyse_semantique/ControleAccesTableau.java
@@ -16,17 +16,40 @@ public class ControleAccesTableau extends ControleSemantique{
 	public void check(ArrayList<TDS> TDSs) throws ErreurSemantique {
 		Expression exp=null;
 		
+		//Type du tableau
+		FieldTableau ft = (FieldTableau) TDS.findIn(TDSs, node.getText(), FieldType.FieldTableau);
+		
+		if(ft == null)
+			throw new ErreurSemantique(node.getLine(), "Tableau non-déclaré : "+node.getText());
+		
+		FieldTypeDef typedeftab = (FieldTypeDefTableau) TDS.findIn(TDSs, ft.getType(), FieldType.FieldTypeDefTableau);
+		String typeelems = ((FieldTypeDefTableau) typedeftab).getTypeElements();
+		
 		//On vérifie le type de tous les fils
-		for (int i = 0; i < node.getChildCount(); i++) {
+		for (int i = 0; i < node.getChildCount()-1; i++) {
 			if(node.getChild(i).getChildCount() == 1){
 				exp = new Expression((CommonTree) node.getChild(i).getChild(0));
+				String type = exp.computeType(TDSs);
+				
+				typedeftab = (FieldTypeDef) TDS.findIn(TDSs, typeelems, FieldType.FieldTypeDefTableau, FieldType.FieldTypeDefStructure);
+				if(typedeftab.getFieldType().equals(FieldType.FieldTypeDefTableau))
+					typeelems = ((FieldTypeDefTableau) typedeftab).getTypeElements();
+								
+				if(!type.equals("int"))
+					throw new ErreurSemantique(node.getLine(), "Acces au tableau "+node.getText()+" par valeur non-entière : "+type);
+			}
+		}
+		
+		//Si le dernier élément est un tableau, on le vérifie
+		CommonTree last = (CommonTree) node.getChild(node.getChildCount()-1);
+		if(typedeftab!=null && typedeftab.getFieldType().equals(FieldType.FieldTypeDefTableau)){
+			if(last.getChildCount() == 1){
+				exp = new Expression((CommonTree) last.getChild(0));
 				String type = exp.computeType(TDSs);
 								
 				if(!type.equals("int"))
 					throw new ErreurSemantique(node.getLine(), "Acces au tableau "+node.getText()+" par valeur non-entière : "+type);
-			
 			}
 		}
-		
 	}
 }
